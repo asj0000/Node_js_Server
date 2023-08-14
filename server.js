003fe81f2f9de58fc1b58af2ba8a8da6f2199b94
Ashish_Jain <121597042+asj0000@users.noreply.github.com>
2023-08-14 09:07:03 +0530

    added data to database

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Product = require('./models/productModel')
 const app = express();
 const port = 3000;
+
+app.use(express.json()); //Creating a middleware
 
  //routes
 
@@ -10,6 +13,20 @@ const port = 3000;
    res.send("HEllo ashish");
 
  })
+
+ //creating a route for user to save data in database
+ app.post('/product' , async(req,res)=>{
+     
+    try{
+        const product  = await Product.create(req.body)  //this saves our data in database
+        res.status(200).json(product)
+      }catch(error){
+        console.log(error.message);
+        res.status(500).json({message : error.message})
+      }
+ })
+
+
 
 mongoose.connect('mongodb+srv://admin:123password@cluster0.mff2whe.mongodb.net/Node-API?retryWrites=true&w=majority')
 .then(()=>{
