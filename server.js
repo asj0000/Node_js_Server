f99b5ec2dbc788c21e84564cf7cd94b8d221709b
Ashish_Jain <121597042+asj0000@users.noreply.github.com>
2023-08-14 11:23:33 +0530

    added a delete request

--- server.js
+++ server.js
@@ -67,6 +67,26 @@ app.use(express.json()); //Creating a middleware
 
  })
 
+ //delete a product
+ app.delete('/products/:id' , async(req,res)=>{
+  try{
+    const {id} = req.params;
+    const product = await Product.findByIdAndDelete(id);
+     
+     //we can't find product by id
+     if(!product){
+       return res.status(404).json({message :  `cannot find product at ${id} id`})
+     }
+     
+     res.status(200).json(product);
+
+ }catch(error){
+ console.log(error.message)
+ res.status(500).json({message : error.message});
+}
+
+ })
+
 
 mongoose.connect('mongodb+srv://admin:123password@cluster0.mff2whe.mongodb.net/Node-API?retryWrites=true&w=majority')
 .then(()=>{
